f716455ee7b95dac0cde198e20dc4c0f6790a99d
tvplidar <tvplidar@gmail.com>
2025-02-20 19:26:24 +0700

    submit

--- app/routes/studentRoute.js
+++ app/routes/studentRoute.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 const studentController = require("../controllers/studentController");
 
-// router.get("/", studentController.getAllStudents);
+router.get("/", studentController.getAllStudents);
 router.get("/:id", studentController.getStudentById);
 router.post("/", studentController.createStudent);
 router.put("/:id", studentController.updateStudent);
